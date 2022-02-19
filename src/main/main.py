89744a5f9fc425ec951eb82b9a15b828a0a4efc0
Felix Schön <mail@fschoen.com>
2022-02-19 16:38:39 +0100

    Update argument parsing

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,7 @@
-import getopt
+import argparse
 import logging
 import sys
 
-from configuration.variables import SUPPORTED_MODES
 from src.main.handler.indesign_handler import InDesignHandler
 from src.main.pipeline import parse_card_list, process_card, process_print
 from src.main.utils.id_generator import generate_ids
@@ -11,44 +10,48 @@ from src.main.utils.info import show_info, Info_Mode
 
 def main(argv):
     config()
+    args = parse_arguments()
 
-    mode = ""
-    deck = ""
+    deck = args.deck
 
-    try:
-        opts, args = getopt.getopt(argv, "m:d:", ["mode=", "deck="])
-    except getopt.GetoptError:
-        show_info("Invalid command line options", mode=Info_Mode.ERROR, end_line=True)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-m", "--mode"):
-            mode = arg
-
-            if mode not in SUPPORTED_MODES:
-                show_info("Mode not supported", mode=Info_Mode.ERROR, end_line=True)
-                return
-        elif opt in ("-d", "--deck"):
-            deck = arg
-        else:
-            show_info("Unknown command line option", mode=Info_Mode.ERROR, end_line=True)
-            return
-
-    if mode == "standard":
-        if deck == "":
+    if args.mode == "standard":
+        if deck is None:
             deck = input("Please enter a deck name: ")
+
         card_entries = parse_card_list("data/decks/" + deck + ".txt")
         indesign_handler = InDesignHandler()
+
         for card_entry in card_entries:
             process_card(card_entry["card"], options=card_entry.get("options"), indesign_handler=indesign_handler)
-        process_print(card_entries)
-    elif mode == "generate_id":
+
+        if not args.no_print:
+            process_print(card_entries)
+    elif args.mode == "generate_id":
         show_info("Generating ID list...")
         generate_ids()
         show_info("Generated ID list", mode=Info_Mode.SUCCESS, end_line=True)
-    elif mode == "debug":
+    elif args.mode == "debug":
         pass
     else:
         show_info("Unknown mode", mode=Info_Mode.ERROR, end_line=True)
+
+
+def parse_arguments():
+    """
+    Parses the program arguments.
+    :return: Parsed arguments
+    """
+    parser = argparse.ArgumentParser(description="Proxy generator for Magic: The Gathering")
+
+    parser.add_argument("mode", choices=["standard", "generate_id", "debug"],
+                        help="Mode of operation of the program")
+    parser.add_argument("--deck", "-d", nargs="?",
+                        help="List of cards to parse, if not provided user will be prompted for list of cards at "
+                             "runtime")
+    parser.add_argument("--no_print", "-np", action="store_true", help="Disables generation of the print templates")
+
+    args = parser.parse_args()
+    return args
 
 
 def config() -> None:
